7582dab930f0903b250aa93c90925444aeaed637
Stephen Wright <stephenwright@Stephens-MBP-2.broadband>
2020-07-13 12:57:47 +0100

    Updates

---
+++ precache-manifest.4241f7a0c9118193e2de2107fddd7896.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44e8dfa1a870f19d25237b140ed99fb",
+    "url": "/tuesdaydotcom/index.html"
+  },
+  {
+    "revision": "46a09b236142071c39ab",
+    "url": "/tuesdaydotcom/static/js/2.b29225e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tuesdaydotcom/static/js/2.b29225e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccead91ba8d0db168373",
+    "url": "/tuesdaydotcom/static/js/main.9e62e877.chunk.js"
+  },
+  {
+    "revision": "f2136b5a88c44bbdf62f",
+    "url": "/tuesdaydotcom/static/js/runtime-main.847c4b4e.js"
+  }
+]);
